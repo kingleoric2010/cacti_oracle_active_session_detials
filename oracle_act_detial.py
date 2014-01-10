12a9f5360c98b2792acba44ed9695ba62a4c4ce3
TonyChyi <tonychee1989@gmail.com>
2014-01-10 09:37:56 +0800

    accurate

--- oracle_act_detial.py
+++ oracle_act_detial.py
@@ -17,7 +17,7 @@ try:
 	con = cx_Oracle.connect(username, password, tbn)
 	cursor = con.cursor()
 
-	cursor.execute("""select * from (select nvl(wait_class,'CPU') activity, trunc(sample_time,'MI') time from v$active_session_history) v pivot ( count(*) for activity in ( 'CPU' as "CPU", 'Concurrency' as "Concurrency", 'System I/O' as "System I/O", 'User I/O' as "User I/O", 'Administrative' as "Administrative", 'Configuration' as "Configuration", 'Application' as "Application", 'Network' as "Network", 'Commit' as "Commit", 'Scheduler' as "Scheduler", 'Cluster' as "Cluster", 'Queueing' as "Queueing", 'Other' as "Other")) where time > sysdate - interval '1' minute""")
+	cursor.execute("""select TIME, trunc("CPU"/60) as "CPU", trunc("Concurrency"/60) as "Concurrency", trunc("System I/O"/60) as "System I/O", trunc("User I/O"/60) as "User I/O", trunc("Administrative"/60) as "Administrative", trunc("Configuration"/60) as "Configuration", trunc("Application"/60) as "Application", trunc("Network"/60) as "Network", trunc("Commit"/60) as "Commit", trunc("Scheduler"/60) as "Scheduler", trunc("Cluster"/60) as "Cluster", trunc("Queueing"/60) as "Queueing", trunc("Other"/60) as "Other" from (select nvl(wait_class,'CPU')  activity, trunc(sample_time,'MI') time from v$active_session_history) v pivot ( count(*) for activity in ( 'CPU' as "CPU", 'Concurrency' as "Concurrency", 'System I/O' as "System I/O", 'User I/O' as "User I/O", 'Administrative' as "Administrative", 'Configuration' as "Configuration", 'Application' as "Application", 'Network' as "Network", 'Commit' as "Commit", 'Scheduler' as "Scheduler", 'Cluster' as "Cluster", 'Queueing' as "Queueing", 'Other' as "Other")) where time > sysdate - interval '1'  minute""")
 	cur = cursor.fetchone()
 
 	cursor.close()
